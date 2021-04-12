f46684a73caf5f12599f6610e5dead80610ba8f2
Shreyas Shukla <shreyas.shukla26@gmail.com>
2021-04-12 18:31:56 +0530

    All Restaurants Screen and Restaurants Info P1

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-
+import { StyleSheet } from 'react-native';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Text>Search</Text>
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: 'green',
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'orangered',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const RestaurantInfo = () => {
+  return (
+    <View>
+      <Text>RestaurantInfo</Text>
+    </View>
+  );
+};
+
+export default RestaurantInfo;
+
+const styles = StyleSheet.create({});
