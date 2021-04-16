ec08cb514014b84e0bd651f480a2a082c1789c14
Shreyas Shukla <shreyas.shukla26@gmail.com>
2021-04-16 22:38:34 +0530

    Restaurant Card Completed and Added FlatList

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-
 import { ThemeProvider } from 'styled-components/native';
+
+import {
+  useFonts as useFontsOswald,
+  Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import {
+  useFonts as useFontsLato,
+  Lato_400Regular,
+} from '@expo-google-fonts/lato';
+
 import { theme } from './src/infrastructure/theme/index';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+
 export default function App() {
+  const [oswaldFontsLoaded] = useFontsOswald({ Oswald_400Regular });
+  const [latoFontsLoaded] = useFontsLato({ Lato_400Regular });
+
+  if (!oswaldFontsLoaded || !latoFontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,34 +1,41 @@
 import React from 'react';
+import { Searchbar } from 'react-native-paper';
+import { StatusBar, FlatList, SafeAreaView, View } from 'react-native';
 import styled from 'styled-components/native';
 
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-import { Searchbar } from 'react-native-paper';
-
-import RestaurantInfoCard from '../components/restaurant-info-card.component';
+import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
-const SearchContainer = styled.View`
+const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+    // backgroundColor: 'green',
+  },
+})``;
 
-export const RestaurantsScreen = () => {
-  return (
-    <SafeArea>
-      <SearchContainer>
-        <Searchbar placeholder="Search" />
-      </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
-    </SafeArea>
-  );
-};
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+    <RestaurantList
+      data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+      //   horizontal
+    />
+  </SafeArea>
+);
